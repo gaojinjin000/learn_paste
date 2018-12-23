826da84b6655ea5ced7f904622a7005f077ee1dd
gaojinjin000 <1127713887@qq.com>
2018-12-23 17:56:58 +0800

    Initial commit

---
+++ server_demon.py
@@ -0,0 +1,9 @@
+from paste.deploy import loadapp
+import os
+from  wsgiref.simple_server import make_server
+
+config = "python_paste.ini"
+appname = "common"
+wsgi_app = loadapp("config:%s" % os.path.abspath(config), appname)
+server = make_server('localhost',80,wsgi_app)
+server.serve_forever()
